d87ae543f2853e2bb1385e1318ca177032758ac2
Alexey Gennadievich <xela07ax@users.noreply.github.com>
2025-04-18 15:53:23 +0300

    optimized my sort
    изменено на for, для меньшего количества сравнений в цикле

--- sort_numbers/srt_num.go
+++ sort_numbers/srt_num.go
@@ -1,16 +1,12 @@
 package sort_numbers
 
 func SortNums(numbers []int) {
-	// необходимо расставить по местам только n – 2 числа
-	for globalNext := 2; globalNext < len(numbers); globalNext++ {
-		var previous int
-		for i, _ := range numbers {
-			if i == len(numbers)-1 {
-				// если дошли до предела, то уходим
-				break
-			} else if i == 0 {
-				previous = numbers[i]
-			}
+	// необходимо расставить по местам только n – 2 числа,
+	// но если придет на вход 2 или 3 числа,
+	// то сортировка не сможет начаться, потому делаем n - 1
+	for globalNext := 1; globalNext < len(numbers); globalNext++ {
+		var previous int = numbers[0]
+		for i := 0; i < len(numbers)-1; i++ { // необходимо сравнить только n – 1 раз
 			next := numbers[i+1]
 			if previous > next {
 				numbers[i] = previous
